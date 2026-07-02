fcc09fb2628bf1395dd4f997e6d68395e8387aa3
yas-hi-la <yashila.amd@gmail.com>
2026-07-02 15:14:51 +0530

    Created Cancel button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var AddTaskButton = document.getElementById("addtask")
 var AddCard = document.getElementById("addcard")
 var BlackScreen = document.getElementById("blackscreen")
+
+var taskname = document.getElementById("TaskName")
+var time = document.getElementById("Deadline")
+var buttons = document.querySelectorAll("#buttons button")
+
+
 AddTaskButton.addEventListener("click",function(){
     AddCard.style.display = "block"
     BlackScreen.style.display = "block"
@@ -10,8 +16,32 @@ var Cancel = document.getElementById("cancel")
 Cancel.addEventListener("click",function(){
     AddCard.style.display = "none"
     BlackScreen.style.display = "none"
+
+    taskname.value = ""
+    time.value = ""
+    buttons.forEach(button => {
+        button.classList.remove("selected")
+    })
 })
 
+var bg = ""
 function highlight(target){
-    target.style.border = "2px solid #161DA3"
+    buttons.forEach(button => {
+        button.classList.remove("selected")
+    })
+    target.classList.add("selected")
+    bg = target.style.backgroundColor
 }
+
+var Add = document.getElementById("add")
+var todocontainer = document.getElementById("first")
+Add.addEventListener("click", function(){
+    var dv = document.createElement("div")
+    dv.classList.add("cards")
+    dv.innerHTML = `<h1>${taskname.value}</h1>
+                    <h2>${time.value}</h2>
+                    <button><p>Delete</p></button>
+                    <button id="edit"><p>Edit</p></button>`
+    dv.style.backgroundColor = bg
+    todocontainer.append(dv)
+})
