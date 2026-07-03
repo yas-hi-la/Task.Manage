85c886608ff04e04660c06cf2eb8b7c4c84cda6e
yas-hi-la <yashila.amd@gmail.com>
2026-07-03 22:29:24 +0530

    Dark mode completed

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var editingcard = null
 var progresscontainer = document.getElementById("second")
 var donecontainer = document.getElementById("third")
 var selected = null
+var Body = document.querySelector("body")
 
 AddTaskButton.addEventListener("click",function(){
     AddCard.style.display = "block"
@@ -33,6 +34,9 @@ function highlight(target){
     buttons.forEach(button => {
         button.classList.remove("selected")
     })
+    if(Body.classList.contains("dark")){
+        target.classList.add("dark")
+    }
     target.classList.add("selected")
     bg = getComputedStyle(target).backgroundColor
 }
@@ -50,6 +54,9 @@ Add.addEventListener("click", function(){
     else{
     var dv = document.createElement("div")
     dv.classList.add("cards")
+    if(Body.classList.contains("dark")){
+        dv.classList.add("dark")
+    }
     dv.draggable = true
     dv.addEventListener("dragstart", function(){
         selected = this
@@ -105,3 +112,98 @@ function edit(target){
         container.appendChild(selected)
     })
     })
+
+var ChangeMode = document.getElementById("mode")
+ChangeMode.addEventListener("click",function(){
+    if(ChangeMode.innerHTML == `<img src="Moon.png">`){
+        ChangeMode.innerHTML = `<img src="solar_sun-bold.png">`
+    }
+    else{
+        ChangeMode.innerHTML = `<img src="Moon.png">`
+    }
+    
+    Body.classList.toggle("dark")
+
+    var NavBar = document.querySelector(".navbar")
+    NavBar.classList.toggle("dark")
+
+    AddTaskButton.classList.toggle("dark")
+    
+    var PlusImg = document.getElementById("plusimg")
+    if(Body.classList.contains("dark")){
+        PlusImg.src = "ic_round-plus-2.png"
+    }
+    else{
+        PlusImg.src = "ic_round-plus.png"
+    }
+
+    var HR = document.querySelector("hr")
+    HR.classList.toggle("dark")
+
+    var DarkTodo = document.querySelector(".todo")
+    DarkTodo.classList.toggle("dark")
+    var DarkProg = document.querySelector(".progress")
+    DarkProg.classList.toggle("dark")
+    var DarkDone = document.querySelector(".done")
+    DarkDone.classList.toggle("dark")
+
+    BlackScreen.classList.toggle("dark")
+
+    AddCard.classList.toggle("dark")
+
+    Add.classList.toggle("dark")
+
+    Cancel.classList.toggle("dark")
+
+    var Button1 = document.getElementById("btn1")
+    var Button2 = document.getElementById("btn2")
+    var Button3 = document.getElementById("btn3")
+    Button1.classList.toggle("dark")
+    Button2.classList.toggle("dark")
+    Button3.classList.toggle("dark")
+
+    var Cards = document.querySelectorAll(".cards")
+    Cards.forEach(card => {
+        card.classList.toggle("dark")
+        if(getComputedStyle(card).backgroundColor == "rgb(230, 240, 250)"){
+            card.style.backgroundColor = "#4BB8FA"
+        }
+        else if(getComputedStyle(card).backgroundColor == "rgb(191, 221, 240)"){
+            card.style.backgroundColor = "#1591DC"
+        }
+        else if(getComputedStyle(card).backgroundColor == "rgb(140, 192, 235)"){
+            card.style.backgroundColor = "#2C5EAD"
+        }
+
+        else if(getComputedStyle(card).backgroundColor == "rgb(75, 184, 250)"){
+            card.style.backgroundColor = "#E6F0FA"
+        }
+        else if(getComputedStyle(card).backgroundColor == "rgb(21, 145, 220)"){
+            card.style.backgroundColor = "#BFDDF0"
+        }
+        else if(getComputedStyle(card).backgroundColor == "rgb(44, 94, 173)"){
+            card.style.backgroundColor = "#8CC0EB"
+        }
+    }) 
+    
+    var Edits = document.querySelectorAll(".edit img")
+    Edits.forEach(Edit => {
+        if(Body.classList.contains("dark")){
+            Edit.src = "Group.png"
+        }
+        else{
+            Edit.src = "edit.png"
+        }
+    })
+
+    Delitems = document.querySelectorAll(".del img")
+    Delitems.forEach(Delete =>{
+        if(Body.classList.contains("dark")){
+            Delete.src = "Vectorcopy.png"
+        }
+        else{
+            Delete.src = "del.png"
+        }
+    })
+})
+
